c349387e59803942153e3132a25a4b6c2219c20c
Vinícius Rodrigues <vinirodrigues712@gmail.com>
2022-03-23 12:07:01 -0300

    Update

---
+++ sintaxe_operadores/script.js
@@ -0,0 +1,41 @@
+function comparaNumeros() {
+    let num1 = Number(prompt('Digite o primeiro valor:'));
+    let num2 = Number(prompt('Digite o segundo valor:'));
+
+    if (!num1 || !num2) {
+        alert('ERRO - Defina dois números!');
+        comparaNumeros();
+    }
+    const primeiraFrase = criaPrimeiraFrase(num1, num2);
+    const segundaFrase = criaSegundaFrase(num1, num2);    
+    return `${primeiraFrase}${segundaFrase}`
+
+    function criaPrimeiraFrase(num1, num2) {
+        let saoIguais = '';
+        if(num1 !== num2) {
+            saoIguais = 'não';
+        }
+
+        return `Os números ${num1} e ${num2}${saoIguais} são iguais. `
+    };
+
+    function criaSegundaFrase(num1, num2) {
+        const soma = num1 + num2;
+
+        let resultado10 = 'menor';
+        let resultado20 = 'menor';
+        const compara10 = soma > 10;
+        const compara20 = soma > 20
+
+        if(compara10) {
+            resultado10 = 'maior';
+        }
+        if(compara20) {
+            resultado20 = 'maior';
+        }
+
+        return `Sua soma é ${soma}, que é ${resultado10} que 10 e ${resultado20} que 20.`
+    }
+
+}
+console.log(comparaNumeros())
