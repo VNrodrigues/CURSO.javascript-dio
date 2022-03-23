b646d4b84d803c6f84e79e22100719912ae6b7d4
Vinícius Rodrigues <vinirodrigues712@gmail.com>
2022-03-23 19:50:03 -0300

    Update

---
+++ módulos/atividade/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Vinícius", 18));
+console.log(mostraCidade("Vinícius", "Araraquara"));
+console.log(mostraHobby("Vinícius", "jogar"));
